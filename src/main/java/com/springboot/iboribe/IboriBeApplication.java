5d3e6e09aeedc260a737557223569b513a50e56d
simGPT <simGPT02@gmail.com>
2026-04-21 16:12:57 +0900

    :tada:Start: ibori 프로젝트 시작

---
+++ src/main/java/com/springboot/iboribe/IboriBeApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.iboribe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IboriBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IboriBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboot/iboribe/IboriBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.iboribe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IboriBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
