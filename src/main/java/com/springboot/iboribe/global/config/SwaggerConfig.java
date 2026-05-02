5dac780f2789e3728785e2e450e917c09b0d01fa
shinchaerin79 <shinchaerin79@gmail.com>
2026-05-03 01:42:01 +0900

    :wrench: Settings: swagger 설정 수정 및 API 문서 구성

--- src/main/java/com/springboot/iboribe/global/config/SwaggerConfig.java
+++ src/main/java/com/springboot/iboribe/global/config/SwaggerConfig.java
@@ -1,33 +1,55 @@
 package com.springboot.iboribe.global.config;
 
 import org.springdoc.core.models.GroupedOpenApi;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class SwaggerConfig {
 
-  @Value("${server.servlet.context-path:}")
-  private String contextPath;
-
   @Bean
   public OpenAPI customOpenAPI() {
-    Server localServer = new Server();
-    localServer.url(contextPath);
-    localServer.setDescription("Local Server");
+    Server localServer = new Server().url("http://localhost:8080").description("Local Server");
 
     return new OpenAPI()
         .addServersItem(localServer)
-        .info(new Info().title("Swagger API 명세서").version("1.0").description("Seoul-Ibori"));
+        .components(
+            new Components()
+                .addSecuritySchemes(
+                    "bearerAuth",
+                    new SecurityScheme()
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT"))
+                .addSecuritySchemes(
+                    "accessTokenCookie",
+                    new SecurityScheme()
+                        .type(SecurityScheme.Type.APIKEY)
+                        .in(SecurityScheme.In.COOKIE)
+                        .name("ACCESS_TOKEN"))
+                .addSecuritySchemes(
+                    "refreshTokenCookie",
+                    new SecurityScheme()
+                        .type(SecurityScheme.Type.APIKEY)
+                        .in(SecurityScheme.In.COOKIE)
+                        .name("REFRESH_TOKEN")))
+        .addSecurityItem(
+            new SecurityRequirement()
+                .addList("bearerAuth")
+                .addList("accessTokenCookie")
+                .addList("refreshTokenCookie"))
+        .info(new Info().title("Ibori API 명세서").version("1.0").description("Seoul-Ibori API 문서"));
   }
 
   @Bean
-  public GroupedOpenApi customGroupedOpenApi() {
-    return GroupedOpenApi.builder().group("api").pathsToMatch("/**").build();
+  public GroupedOpenApi apiGroup() {
+    return GroupedOpenApi.builder().group("api").pathsToMatch("/api/**").build();
   }
 }
