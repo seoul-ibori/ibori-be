9e7be83e29f0ba8241d229dee683ace41afb87ac
shinchaerin79 <shinchaerin79@gmail.com>
2026-05-03 01:43:48 +0900

    :sparkles: Feat: auth JWT 기반 사용자 인증 API 구현

---
+++ src/main/java/com/springboot/iboribe/domain/user/entity/User.java
@@ -0,0 +1,27 @@
+package com.springboot.iboribe.domain.user.entity;
+
+import jakarta.persistence.*;
+
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Table(name = "users")
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false, length = 30)
+  private String name;
+
+  @Column(nullable = false, unique = true, length = 50)
+  private String username;
+
+  @Column(nullable = false)
+  private String password;
+}
