dd393f86be9cceecf2422da808bdf740297f2677
simGPT <simGPT02@gmail.com>
2026-05-06 13:46:10 +0900

    :bug:Fix:WebClient.Builder 및 ObjectMapper 빈 직접 등록으로 CodefClient 기동    오류 수정

--- src/main/java/com/springboot/iboribe/global/config/WebClientConfig.java
+++ src/main/java/com/springboot/iboribe/global/config/WebClientConfig.java
@@ -4,11 +4,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @Configuration
 public class WebClientConfig {
 
   @Bean
   public WebClient.Builder webClientBuilder() {
     return WebClient.builder();
+  }
+
+  @Bean
+  public ObjectMapper objectMapper() {
+    return new ObjectMapper();
   }
 }
