db5ceb50462cfee04413bb5ffa4febfccf2be3cf
simGPT <simGPT02@gmail.com>
2026-05-08 19:32:10 +0900

    :recycle:Refactor: Cors 설정 하드코딩으로 수정

--- src/main/java/com/springboot/iboribe/global/config/CorsConfig.java
+++ src/main/java/com/springboot/iboribe/global/config/CorsConfig.java
@@ -2,7 +2,6 @@ package com.springboot.iboribe.global.config;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -11,14 +10,16 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @Configuration
 public class CorsConfig {
 
-  @Value("${cors.allowed-origins}")
-  private List<String> allowedOrigins;
-
   @Bean
   public UrlBasedCorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
 
-    configuration.setAllowedOrigins(allowedOrigins);
+    configuration.setAllowedOrigins(
+        List.of(
+            "http://localhost:3000",
+            "http://localhost:5173",
+            "http://localhost:5174",
+            "https://ibori.site"));
     configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
     configuration.setAllowedHeaders(List.of("*"));
     configuration.setAllowCredentials(true);
