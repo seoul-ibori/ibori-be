0ba33745ae346bd4f0454960ea2f1d2d9d7817e6
simGPT <simGPT02@gmail.com>
2026-05-08 18:42:29 +0900

    🚀 Deploy: Release 0.1.4 (CORS 에러 수정) (#32)
    * :sparkles:Feat: global 공통 인프라 초기 구성 (#6)
    
    * ✨Feat: Auth JWT 기반 사용자 인증 기능 추가
    
    * :heavy_plus_sign: Dependency: upgrade jjwt to 0.12.5 (JWT 최신 버전 적용)
    
    * :wrench: Settings: security cors jwt properties 설정 추가
    
    * :sparkles: Feat: JWT provider filter cookie 인증 기능 추가
    
    * :recycle: Refactor: 응답 및 예외 처리 구조 정리
    
    * :wrench: Settings: swagger 설정 수정 및 API 문서 구성
    
    * :wrench: Settings: configuration properties 및 JPA auditing 설정 추가
    
    * :sparkles: Feat: auth JWT 기반 사용자 인증 API 구현
    
    * :wrench: Settings: submodule update
    
    * ✨Feat: CODEF 자녀 등록 API (1차/2차 인증) 및 연동 구조 구현
    
    * :heavy_plus_sign: Dependency: WebClient 의존성 추가
    
    * :sparkles: Feat: CODEF 도메인 패키지 구조 생성
    
    * :wrench: Settings: submodule update
    
    * ✨Feat: CODEF 자녀 목록 조회 및 진료/투약정보 API 연동
    
    * :sparkles: Feat: CODEF 자녀 목록 및 진료/투약정보 DTO 추가
    
    * :sparkles: Feat: CODEF 자녀 목록 및 진료/투약정보 API 호출 로직 추가
    
    * :sparkles: Feat: CODEF 자녀 목록 및 진료/투약정보 서비스 로직 추가
    
    * :sparkles: Feat: CODEF 자녀 목록 및 진료/투약정보 API 엔드포인트 추가
    
    * 🐛 Fix: WebClient.Builder 빈 직접 등록으로 CodefClient 기동 오류 수정 (#23)
    
    * :wrench:Settings: cd 워크플로우, 이미지 변경 시 컨테이너 강제 재시작 옵션 추가
    
    * :bug:Fix: WebClient.Builder 빈 직접 등록으로 CodefClient 기동 오류 수정
    
    * :bug:Fix:WebClient.Builder 및 ObjectMapper 빈 직접 등록으로 CodefClient 기동
       오류 수정
    
    * 🔧 Infra: 서버 접속 시 502 에러 발생 (#26)
    
    * :wrench:Settings: codef 관련 환경변수 추가
    
    * :wrench:Settings: 서브 모듈 수정
    
    * :wrench:Settings: docker-compose.yml 수정에 따른 수정
    
    * :wrench:Settings: 서브모듈 수정 (#28)
    
    * :wrench:Settings: CorsConfig 수정 (#31)
    
    ---------
    
    Co-authored-by: shinchaerin79 <shinchaerin79@gmail.com>

--- src/main/java/com/springboot/iboribe/global/config/CorsConfig.java
+++ src/main/java/com/springboot/iboribe/global/config/CorsConfig.java
@@ -2,6 +2,7 @@ package com.springboot.iboribe.global.config;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -10,12 +11,14 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @Configuration
 public class CorsConfig {
 
+  @Value("${cors.allowed-origins}")
+  private List<String> allowedOrigins;
+
   @Bean
   public UrlBasedCorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
 
-    configuration.setAllowedOrigins(
-        List.of("http://localhost:3000", "http://localhost:5173", "http://localhost:5174"));
+    configuration.setAllowedOrigins(allowedOrigins);
     configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
     configuration.setAllowedHeaders(List.of("*"));
     configuration.setAllowCredentials(true);
